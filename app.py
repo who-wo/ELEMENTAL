2cd62bddfb3855249a3e48318c979f02d391cca2
XX II <thanachitkrisana@gmail.com>
2026-02-10 13:28:12 +0700

    Fix port and production settings

--- app.py
+++ app.py
@@ -208,4 +208,6 @@ def order(product_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # ดึงค่า PORT จากระบบที่ Render กำหนดมาให้ ถ้าหาไม่เจอจะใช้ 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
